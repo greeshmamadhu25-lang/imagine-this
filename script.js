90d8e02b610892f26a70ba0aa7c9ef4e9241d531
greeshmamadhu25-lang <greeshmamadhu25@gmail.com>
2026-08-20 14:42:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,5 @@
 const beginBtn = document.getElementById("beginBtn");
 
-beginBtn.addEventListener("click", function () {
-    document.body.classList.add("fade-out");
-
-    setTimeout(function () {
-        window.location.href = "story.html";
-    }, 800);
-});
-const beginBtn = document.getElementById("beginBtn");
-
 if (beginBtn) {
     beginBtn.addEventListener("click", function () {
         document.body.classList.add("fade-out");
